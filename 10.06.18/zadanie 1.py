8f2bc5516c811d041e55586da7794fd571650c54
Magdalena Sztuk <kimagdalenasztuk@gmail.com>
2018-06-10 10:42:08 +0200

    zadanie1

---
+++ 10.06.18/zadanie 1.py
@@ -0,0 +1,14 @@
+
+def konwersja_odleglosci(metry):
+    km =int(metry / 1000)
+    mile = float(metry / 1608)
+    nm = float(metry / 1852)
+
+
+    return {
+    'kilometers': km,
+    'miles': mile,
+    'nautical miles': nm,
+    'all': [km, mile, nm]
+     }
+
